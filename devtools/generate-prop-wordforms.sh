e44bc5c16e82bde7baba893d3ae65026e31d8c61
Sjur N Moshagen <sjurnm@mac.com>
2024-01-24 11:04:24 +0200

    Adjust path after fst build reorg

--- devtools/generate-prop-wordforms.sh
+++ devtools/generate-prop-wordforms.sh
@@ -22,13 +22,13 @@ fi
 morf_codes="+N+Prop+Utr+Sg+Indef"
 
 # Lexicon source file for lexicons and lemmas:
-source_file=src/fst/stems/rmu-propernouns.lexc
+source_file=src/fst/morphology/stems/rmu-propernouns.lexc
 
 # Lexicons that should NOT be used to extract lemmas (egrep expression):
 exception_lexicons="(flagK)"
 
 # FST used for generation, MINUS suffix:
-generator_file=src/generator-gt-norm
+generator_file=src/fst/generator-gt-norm
 
 # How many lemmas maximally for each lexicon:
 lemmacount=10
